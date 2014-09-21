bc57a942e0c52fbd4a4be09efae026f21f0aa43c
Rezaur Chowdhury <rezaurc@yahoo.com>
2014-09-21 07:19:13 +0600

    added feature post to single

--- content-single.php
+++ content-single.php
@@ -14,7 +14,11 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php the_content(); ?>
+		<?php 
+                            if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                the_post_thumbnail();
+            } 
+                the_content(); ?>
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'boot_Strap' ),
