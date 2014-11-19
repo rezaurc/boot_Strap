5eee0517eb30428b7c8ec79b551b0c378b009222
Rezaur Chowdhury <rezaurc@yahoo.com>
2014-11-20 04:08:23 +0600

    mobile menu fix

--- footer.php
+++ footer.php
@@ -15,9 +15,9 @@
 			<?php do_action( 'boot_Strap_credits' ); ?>
 			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'boot_Strap' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'boot_Strap' ), 'boot_Strap', '<a href="http://www.rezaur.com" rel="designer">Rezaur Chowdhury</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'boot_Strap' ), 'boot_Strap', '<a href="http://www.rezaur.com" rel="author">Rezaur Chowdhury</a>' ); ?>
 		</div><!-- .site-info -->
-            <a href="#" class="back-to-top"><i class="glyphicon glyphicon-chevron-up"></i> Back to Top</a>
+            <a href="#" class="back-to-top"><i class="glyphicon glyphicon-chevron-up"></i><?php printf (' Back to Top','boot_Strap');?></a>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <?php wp_footer(); ?>
